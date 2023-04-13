a18f1ce19bccf36a7d7fdd679944978bec3f3d88
felipnrv <77867598+felipnrv@users.noreply.github.com>
2023-04-13 16:57:53 +0000

    version 1.2

--- Ejercicios.py
+++ Ejercicios.py
@@ -3,4 +3,15 @@ print("Hello world")
 """Comentarios
 Test
 """
+user="felipe"
+#f sirve para dar formato y que se imprima la variable 
+print(f'{user}')
+#2 ejercicio 
+nom="felipe"
+apellido="garcia"
+variable="Mi nombre es {} y mi apellido {}".format(nom,apellido)
+print(variable)
 
+lives=4
+lives -=1
+print(lives)
