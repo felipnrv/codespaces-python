f5f5bae58b748ef1fe6195d60410aae9397fd38c
felipnrv <77867598+felipnrv@users.noreply.github.com>
2023-04-13 17:43:26 +0000

    version 1.3

--- Ejercicios.py
+++ Ejercicios.py
@@ -11,7 +11,28 @@ nom="felipe"
 apellido="garcia"
 variable="Mi nombre es {} y mi apellido {}".format(nom,apellido)
 print(variable)
-
+# 3 ejercicio
 lives=4
 lives -=1
 print(lives)
+#(%) Modulo o Residuo
+#(//) Division con valor entero
+#(**) Exponenciación
+y=4.161592653589793
+x=2.001213456789
+y_str=format(y,".2g")
+print(y_str)
+
+tolerancia=0.00001
+print(abs(y-x)>tolerancia)
+
+txt="Hola y chao"
+print("Hola" in txt)
+
+size=len('hello world    ')
+print(size)
+
+print(txt.swapcase())
+print(txt.startswith('Ho'))
+print(txt.endswith('ao'))
+
